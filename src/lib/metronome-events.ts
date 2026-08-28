363894f4ac8df6ed799c569138fe384f68266cb0
Leonid Trofymchuk <stesel23@gmail.com>
2026-08-28 23:31:44 +0300

    Include lesson identity in exercise metadata

--- src/lib/metronome-events.ts
+++ src/lib/metronome-events.ts
@@ -3,4 +3,6 @@ export const START_EXERCISE_EVENT = "guitar-grok:start-exercise";
 export interface StartExerciseDetail {
   exerciseId: string;
   exerciseTitle: string;
+  lessonId: string;
+  lessonTitle: string;
 }
