3b88fd4216f13dcc15dcad3f017e97c01be06c56
Leonid Trofymchuk <stesel23@gmail.com>
2026-08-29 13:14:17 +0300

    Prepare exercise before starting metronome

--- src/lib/metronomeEvents.ts
+++ src/lib/metronomeEvents.ts
@@ -1,4 +1,4 @@
-export const START_EXERCISE_EVENT = "guitar-grok:start-exercise";
+export const PREPARE_EXERCISE_EVENT = "guitar-grok:prepare-exercise";
 
 export interface StartExerciseDetail {
   exerciseId: string;
